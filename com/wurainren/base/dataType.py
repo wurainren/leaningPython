008a1616ab770ea58de4ee18f96e8d6c12ee4e0f
任武 <ex-renwu@dashuf.com>
2017-10-30 17:34:12 +0800

    数据容器

--- com/wurainren/base/dataType.py
+++ com/wurainren/base/dataType.py
@@ -1,5 +1,32 @@
 # -*- coding:utf-8 -*-
 
+#Python内置的一种数据类型是列表：list。
+# list是一种有序的集合，可以随时添加和删除其中的元素。
+
+classmates = ['Michael', 'Bob', 'Tracy']
+# query 通过下标；切片
+print(classmates[0])
+print(classmates[-1])
+print(classmates[:1])  #===》这里切出来的结果，还是一个list
+
+# count 通过len()函数 获取list元素的个数
+print(len(classmates))
+
+# add 通过append()函数，追加元素到末尾；insert()函数 将元素插入指定的位置
+classmates.append('Adem')
+print(classmates)
+classmates.insert(1,'Good')
+print(classmates)
+classmates[1]='Jack'
+print(classmates)
+
+# delete 通过pop()函数，删除list末尾；也可用指定位置删除元素
+classmates.pop()
+print(classmates)
+classmates.pop(1)
+print(classmates)
+
+
 L =[
     ['Apple','Google','Microsoft'],
     ['Java','Python','Ruby','PHP'],
@@ -9,3 +36,13 @@ L =[
 print(L[0][0])
 print(L[1][1])
 print(L[2][2])
+
+
+#有序列表元组：tuple。
+# tuple和list非常类似，但是tuple一旦初始化就不能修改
+
+
+
+
+
+#list和tuple是Python内置的有序集合，一个可变，一个不可变。根据需要来选择使用它们
