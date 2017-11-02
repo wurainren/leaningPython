0b151f5602cf30db1347ce09f692f6df2956e6c0
任武 <ex-renwu@dashuf.com>
2017-11-02 11:15:29 +0800

    新增内容

--- com/wurainren/base/base.py
+++ com/wurainren/base/base.py
@@ -203,3 +203,86 @@ def power2(x, n=2):
     return s
 print(power2(5)) #25
 
+
+# 3.可变参数 在入参前面加入一个“ * ”
+
+# 不可变参数
+# def calc(numbers):
+#     sum =0 
+#     for n in numbers:
+#         sum = sum + n*n
+#     return sum 
+
+# print(calc([1,2,3]))        #14
+# print(calc((1,3,5,7)))      #84
+
+# 可变参数
+def calc(*numbers):
+    sum=0
+    for n in numbers:
+        sum = sum + n*n
+    return sum 
+print(calc(1,2))    #5
+print(calc())       #0
+
+nums = [1, 2, 3]
+print(calc(nums[0],nums[1],nums[2])) #14
+
+print(calc(*nums)) # 等价上面，在list或tuple前面加一个*号，把list或tuple的元素变成可变参数传进去
+                   # *nums表示把nums这个list的所有元素作为可变参数传进去。这种写法相当有用，而且很常见
+
+# 4.关键字参数（**kw，kw可以是其他名称）   关键字参数允许你传入0个或任意个含参数名的参数，这些关键字参数在函数内部自动组装为一个dict
+# def person(name,age,**kw):
+#     print('name:',name,'age:',age,'other:',kw)
+
+# print(person('lishang',23))
+# print(person('zhangyong',20, city='shenzhen'))
+
+# extra = {'city': 'Beijing', 'job': 'Engineer'}
+# print(person('Maike',25,**extra))
+#**extra表示把extra这个dict的所有key-value用关键字参数传入到函数的**kw参数，kw将获得一个dict，注意kw获得的dict是extra的一份拷贝，对kw的改动不会影响到函数外的extra
+
+# 5.命名关键字参数 （命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数）
+#对于关键字参数，函数的调用者可以传入任意不受限制的关键字参数。至于到底传入了哪些，就需要在函数内部通过kw检查
+# def person(name,age,**kw):
+#     if 'city' in kw:
+#         pass
+#     if 'job' in kw:
+#         pass
+#     print('name',name,'age',age,'other:',kw)
+
+# print(person('Jack',24,city='Beijing',addr='Chayang',zipcode=123456)) #name Jack age 24 other: {'city': 'Beijing', 'addr': 'Chayang', 'zipcode': 123456}
+
+#如果要限制关键字参数的名字，就可以用命名关键字参数，例如，只接收city和job作为关键字参数。
+#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
+# def person(name, age, *, city, job):
+#     print(name, age, city, job)
+
+# print(person('Jack', 24, city='Beijing', job='Engineer')) #Jack 24 Beijing Engineer
+
+#如果函数定义中已经有了一个可变参数，后面跟着的命名关键字参数就不再需要一个特殊分隔符*了：
+def person(name,age,*args,city,job):
+    print(name,age,args,city,job)
+
+#命名关键字参数必须传入参数名，这和位置参数不同。如果没有传入参数名，调用将报错
+person('Jack',23,'Beijing','Engineer');
+'''
+Traceback (most recent call last):
+  File "base.py", line 267, in <module>
+    person('Jack',23,'Beijing','Engineer');
+TypeError: person() missing 2 required keyword-only arguments: 'city' and 'job'
+
+'''
+
+#参数组合
+#在Python中定义函数，可以用必选参数、默认参数、可变参数、关键字参数和命名关键字参数，这5种参数都可以组合使用。
+# 但是请注意，参数定义的顺序必选是：必选参数、默认参数、可变参数、命名关键字参数和关键字参数
+def f1(a,b,c=0,*args,**kw):
+    print('a=',a,'b=',b,'c=',c,'args=',args,'kw=',kw)
+
+def f2(a,b,c=0,*,d,**kw):
+    print('a=',a,'b=',b,'c=',c,'d=',d,'kw=',kw)
+
+
+
+
